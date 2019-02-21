3fea05d45216c1e6212f4806795be3807cfdc33a
Thomas Theiner <thomas.theiner@gmx.de>
2019-02-21 14:20:37 +0100

    Upated Gradle versions and app build settings, changed Radio912 strategy to JSON

--- app/src/main/java/org/theiner/tinyradio/strategy/Radio912Strategy.java
+++ app/src/main/java/org/theiner/tinyradio/strategy/Radio912Strategy.java
@@ -1,6 +1,8 @@
 package org.theiner.tinyradio.strategy;
 
 import org.apache.commons.lang3.text.WordUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.jsoup.nodes.Document;
 import org.theiner.tinyradio.util.HTTPHelper;
 
@@ -11,27 +13,24 @@ import org.theiner.tinyradio.util.HTTPHelper;
 public class Radio912Strategy implements SongTitleStrategy {
 
     //private final String url = "http://radio912web.lokalradioservices.de/playlist/last5.php";
-    private final String url = "http://www.radio912.de/";
+    private final String url = "https://api-prod.nrwlokalradios.com/playlist/current?station=6";
 
     @Override
     public String getSongTitle(HTTPHelper myHttpHelper) {
         String ergebnis = "Unbekannt";
 
-        Document radio912Doc = myHttpHelper.getDocumentFromUrl(url, "", false);
+        String radio912Json = myHttpHelper.getHtmlFromUrl(url, "", false, false);
 
-        if(radio912Doc != null) {
-//            Element playlist = radio912Doc.getElementById("playlist");
-//            String artist = playlist.getElementsByClass("artist").get(0).text();
-//            String title = playlist.getElementsByClass("title").get(0).text();
+        try {
+            JSONObject radio912Obj = new JSONObject(radio912Json);
 
-            String artist = radio912Doc.getElementsByClass("stichzeile").get(0).text();
-            String title = radio912Doc.getElementsByClass("headline").get(0).text();
+            String artist = radio912Obj.getString("artist");
+            String title = radio912Obj.getString("title");
 
             ergebnis = artist + " - " + title;
-
-            ergebnis = WordUtils.capitalizeFully(ergebnis);
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
         return ergebnis;
     }
 }
